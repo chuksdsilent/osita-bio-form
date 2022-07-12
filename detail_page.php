9a949c627b614878aa725a533605ee219c2284a8
Oshaba Samson <chuksdsilent@gmail.com>
2022-07-12 09:31:50 +0100

    changed the search output format

--- detail_page.php
+++ detail_page.php
@@ -49,101 +49,100 @@
       <h4>CBSS BIO-DATA FORM</h4>
     </section>
     <main class="container">
-      <form class="row g-3">
         <h6 class="sectionA fw-bold">
           PERSONAL INFORMATION
         </h6>
         <div class="row">
           <div class="col-6">
             <div class="row">
-              <div class="col-12 mt-2">
+              <div class="col-12 mt-2 fw-bold">
                 Surname
               </div>
-              <div class="col-12 mt-2 fw-bold">
+              <div class="col-12 mt-2 ">
                 <?php echo $row["inputfirstname"]; ?>
               </div>
             </div>
             <div class="row">
-              <div class="col-12 mt-2">
+              <div class="col-12 mt-2 fw-bold">
                 Other Names
               </div>
-              <div class="col-12 mt-2 fw-bold">
+              <div class="col-12 mt-2">
                 <?php echo $row["inputothernames"]; ?>
               </div>
             </div>
             <div class="row">
-              <div class="col-12 mt-2">
+              <div class="col-12 mt-2 fw-bold">
                 Address 1
               </div>
-              <div class="col-12 mt-2 fw-bold">
+              <div class="col-12 mt-2">
                 <?php echo $row["inputAddress"]; ?>
               </div>
             </div>
             <div class="row">
-              <div class="col-12 mt-2">
+              <div class="col-12 mt-2 fw-bold">
                 Address 2
               </div>
-              <div class="col-12 mt-2 fw-bold">
+              <div class="col-12 mt-2">
                 <?php echo $row["inputAddress2"]; ?>
               </div>
             </div>
           </div>
-          <div class="col-6">
+          <div class="col-6 ">
             <img src="<?php echo $row["passport"]; ?>" style="width: 200px; height: 200px; object-fit: cover;" alt="">
           </div>
         </div>
         <div class="col-md-4">
-          <div class="col mt-2">
+          <div class="col mt-2 fw-bold">
             Country
           </div>
-          <div class="col mt-2 fw-bold">
+          <div class="col mt-2">
             <?php echo $row["inputcountry"]; ?>
           </div>
         </div>
         <div class="col-md-4">
-          <div class="col mt-2">
+          <div class="col mt-2 fw-bold">
             Choose State
           </div>
-          <div class="col mt-2 fw-bold">
+          <div class="col mt-2">
             <?php echo $row["inputState"]; ?>
           </div>
 
         </div>
         <div class="col-md-4">
-          <div class="col mt-2">
+          <div class="col mt-2 fw-bold">
             Local Govt
           </div>
-          <div class="col mt-2 fw-bold">
+          <div class="col mt-2">
             <?php echo $row["inputlocalgovt"]; ?>
           </div>
 
         </div>
         <div class="col-12">
-          <div class="col mt-2">
+          <div class="col mt-2 fw-bold">
             Email Address
           </div>
-          <div class="col mt-2 fw-bold">
+          <div class="col mt-2 ">
             <?php echo $row["inputemail"]; ?>
           </div>
         </div>
         <div class="col-md-4">
-          <div class="col mt-2">
+          <div class="col mt-2 fw-bold">
             Gender
           </div>
-          <div class="col mt-2 fw-bold">
+          <div class="col mt-2">
             <?php echo $row["inputgender"]; ?>
           </div>
         </div>
         <div class="col-md-4">
 
-          <div class="col mt-2">
+          <div class="col mt-2 fw-bold">
             Marital Status
           </div>
-          <div class="col mt-2 fw-bold">
+          <div class="col mt-2">
             <?php echo $row["inputmaritalstatus"]; ?>
           </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-4 fw-bold">
           <!-- Date input -->
           Date Of Birth:
         </div>
@@ -153,14 +152,14 @@
           QUALIFICATIONS OBTAINED BEFORE RESUMING WORK AT NASRDA
         </h6>
         <div class="col-md-8">
-          <div class="col mt-2 fw-bold">
+          <div class="col mt-2">
             <?php echo $row["checkPG"]; ?>
           </div>
         </div>
         <div class="col-md-8">
           <!-- FOR UNDERGRADUATE -->
 
-          <div class="col mt-2 fw-bold">
+          <div class="col mt-2">
             <?php echo $row["checkUNDERG"]; ?>
           </div>
         </div>
@@ -172,34 +171,34 @@
           <!-- FOR FSLC -->
         </div>
         <div class="col-12">
-          <div class="col mt-2">
+          <div class="col mt-2 fw-bold">
             Institution Attended with date(POSTGRADUATE)
           </div>
-          <div class="col mt-2 fw-bold">
+          <div class="col mt-2">
             <?php echo $row["inputPG"]; ?>
           </div>
         </div>
         <div class="col-12">
-          <div class="col mt-2">
+          <div class="col mt-2 fw-bold">
             Institution Attended with date(Undergraduate)
           </div>
-          <div class="col mt-2 fw-bold">
+          <div class="col mt-2">
             <?php echo $row["inputUNDERG"]; ?>
           </div>
         </div>
         <div class="col-12">
-          <div class="col mt-2">
+          <div class="col mt-2 fw-bold">
             Institution Attended with date(SSCE)
           </div>
-          <div class="col mt-2 fw-bold">
+          <div class="col mt-2">
             <?php echo $row["inputSSCE"]; ?>
           </div>
         </div>
         <div class="col-12">
-          <div class="col mt-2">
+          <div class="col mt-2 fw-bold">
             Institution Attended with date(FSLC)
           </div>
-          <div class="col mt-2 fw-bold">
+          <div class="col mt-2">
             <?php echo $row["inputFSLC"]; ?>
           </div>
         </div>
@@ -217,46 +216,46 @@
         </div>
         <div class="col-md-4">
 
-          <div class="col mt-2">
+          <div class="col mt-2 fw-bold">
             IPPIS Number
           </div>
-          <div class="col mt-2 fw-bold">
+          <div class="col mt-2">
             <?php echo $row["inputIPPIS"]; ?>
           </div>
         </div>
         <div class="col-md-4">
 
-          <div class="col mt-2">
+          <div class="col mt-2 fw-bold">
             Bank Name
           </div>
-          <div class="col mt-2 fw-bold">
+          <div class="col mt-2">
             <?php echo $row["inputBankName"]; ?>
           </div>
         </div>
         <div class="col-md-4">
 
-          <div class="col mt-2">
+          <div class="col mt-2 fw-bold">
             Account Name
           </div>
-          <div class="col mt-2 fw-bold">
+          <div class="col mt-2">
             <?php echo $row["inputAccountNumber"]; ?>
           </div>
         </div>
         <div class="col-md-4">
 
-          <div class="col mt-2">
+          <div class="col mt-2 fw-bold">
             Account Number:
           </div>
-          <div class="col mt-2 fw-bold">
+          <div class="col mt-2">
             <?php echo $row["inputAccountNumber"]; ?>
           </div>
         </div>
         <div class="col-md-4">
 
-          <div class="col mt-2">
+          <div class="col mt-2 fw-bold">
             Account Type
           </div>
-          <div class="col mt-2 fw-bold">
+          <div class="col mt-2">
             <?php echo $row["inputAccountType"]; ?>
           </div>
         </div>
@@ -274,61 +273,61 @@
         </div>
         <div class="col-md-4">
 
-          <div class="col mt-2">
+          <div class="col mt-2 fw-bold">
             File Number
           </div>
-          <div class="col mt-2 fw-bold">
+          <div class="col mt-2">
             <?php echo $row["file_no"]; ?>
           </div>
         </div>
         <div class="col-md-4">
 
-          <div class="col mt-2">
+          <div class="col mt-2 fw-bold">
             Date Of First Appointment
           </div>
-          <div class="col mt-2 fw-bold">
+          <div class="col mt-2">
             <?php echo date_format(date_create($row["dateFirstAp"]), "d M, Y"); ?>
           </div>
         </div>
         <div class="col-md-4">
-          <div class="col mt-2">
+          <div class="col mt-2 fw-bold">
             Date Of Assuption
           </div>
-          <div class="col mt-2 fw-bold">
+          <div class="col mt-2">
             <?php echo date_format(date_create($row["dateofAssuption"]), "d M, Y"); ?>
           </div>
         </div>
         <div class="col-md-4">
-          <div class="col mt-2">
+          <div class="col mt-2 fw-bold">
             Date Of Present Appointment
           </div>
-          <div class="col mt-2 fw-bold">
+          <div class="col mt-2">
             <?php echo date_format(date_create($row["datePresentAp"]), "d M, Y"); ?>
           </div>
         </div>
         <div class="col-md-4">
 
 
-          <div class="col mt-2">
+          <div class="col mt-2 fw-bold">
             Date Of Regularization
           </div>
-          <div class="col mt-2 fw-bold">
+          <div class="col mt-2">
             <!-- <?php echo date_format(date_create($row["datePresentAp"]), "d M, Y"); ?> -->
           </div>
         </div>
         <div class="col-md-4">
-          <div class="col mt-2">
+          <div class="col mt-2 fw-bold">
             Date Of Confirmation
           </div>
-          <div class="col mt-2 fw-bold">
+          <div class="col mt-2">
             <?php echo date_format(date_create($row["dateofConfirm"]), "d M, Y"); ?>
           </div>
         </div>
         <div class="col-md-8">
-          <div class="col mt-2">
+          <div class="col mt-2 fw-bold">
             Any Transfer/Merge of Service
           </div>
-          <div class="col mt-2 fw-bold">
+          <div class="col mt-2">
             <?php echo $row["AnyTransfer"]; ?>
           </div>
         </div>
@@ -337,52 +336,52 @@
         </div>
         <div class="row">
           <div class="col-12">
-            <div class="col mt-2">
+            <div class="col mt-2 fw-bold">
               Place of Primary Assignment:
             </div>
-            <div class="col mt-2 fw-bold">
+            <div class="col mt-2">
               <?php echo $row["PlaceofAp"]; ?>
             </div>
           </div>
           <div class="col-12">
-            <div class="col mt-2">
+            <div class="col mt-2 fw-bold">
               Department
             </div>
-            <div class="col mt-2 fw-bold">
+            <div class="col mt-2">
               <?php echo $row["inputDepartment"]; ?>
             </div>
           </div>
           <div class="col-12">
-            <div class="col mt-2">
+            <div class="col mt-2 fw-bold">
               Division
             </div>
-            <div class="col mt-2 fw-bold">
+            <div class="col mt-2">
               <?php echo $row["inputDivision"]; ?>
             </div>
           </div>
           <div class="col-12">
 
-            <div class="col mt-2">
+            <div class="col mt-2 fw-bold">
               Unit
             </div>
-            <div class="col mt-2 fw-bold">
+            <div class="col mt-2">
               <?php echo $row["inputUnit"];; ?>
             </div>
           </div>
           <div class="col-12">
-            <div class="col mt-2">
+            <div class="col mt-2 fw-bold">
               Schedule of Duty
             </div>
-            <div class="col mt-2 fw-bold">
+            <div class="col mt-2">
               <?php echo $row["inputSchedule"]; ?>
             </div>
           </div>
           <div class="col-12">
 
-            <div class="col mt-2">
+            <div class="col mt-2 fw-bold">
               Any Ad-hoc Duty
             </div>
-            <div class="col mt-2 fw-bold">
+            <div class="col mt-2">
               <?php echo $row["inputAdhoc"]; ?>
             </div>
           </div>
@@ -397,19 +396,19 @@
           </div>
           <div class="col-12">
 
-            <div class="col mt-2">
+            <div class="col mt-2 fw-bold">
               Training Attended Since Appointment
             </div>
-            <div class="col mt-2 fw-bold">
+            <div class="col mt-2">
               <?php echo $row["inputTraining"]; ?>
             </div>
           </div>
         </div>
         <div class="col-12">
-          <button type="submit" class="btn btn-primary">Print</button>
+          <button type="submit" class="btn btn-primary"  onclick="window.print()">Print</button>
         </div>
         <p></p>
-      </form>
+      
     </main>
     <footer class="container">
       <div>
